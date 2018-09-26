658d5786ca1020fd4b54b742e51ba0a77d1fe636
unknown <drugoralex@gmail.com>
2018-09-25 23:48:13 -0500

    Tengo_sueño

---
+++ VisorApp/VisorCore/src/main/java/pe/com/core/business/UserBusiness.java
@@ -0,0 +1,38 @@
+package pe.com.core.business;
+
+import java.util.List;
+
+import pe.com.core.dao.CategoriaDao;
+import pe.com.core.dao.UserDao;
+import pe.com.core.entity.Categoria;
+import pe.com.core.entity.User;
+
+public class UserBusiness {
+
+private final UserDao userDao = new UserDao();
+	
+	public void insertar(User user) throws Exception{
+		this.userDao.insertar(user);
+	}
+	
+	public void actualizar(User user) throws Exception{
+		this.userDao.actualizar(user);
+	}
+	
+	public void eliminar(User user) throws Exception{
+		this.userDao.eliminar(user);
+	}
+	
+	public User obtener(Integer idUser)throws Exception{
+		return this.userDao.obtener(new User(idUser));
+	}
+	
+	public List<User> listar() throws Exception{
+		return this.userDao.listar();
+	}
+	
+	public List<User> listar(String firstname) throws Exception{
+		return this.userDao.listar(firstname.trim().toUpperCase());
+	}
+	
+}
